746d692d1bc1a3a85380f75cbfd573a0f076f227
Pavan Gaikwad <gaikwadpavan57@gmail.com>
2025-06-15 20:22:16 +0530

    responsive changes

--- src/Project5 Recipe App/Header.jsx
+++ src/Project5 Recipe App/Header.jsx
@@ -1,34 +1,29 @@
 import { useEffect, useState } from "react";
-import "./Header.css";
+import './Header.css';
+
 const Header = () => {
-  const [randamfood, setRandamfood] = useState([]);
+  const [randomFood, setRandomFood] = useState([]);
+
   useEffect(() => {
-    fetch("https://www.themealdb.com/api/json/v1/1/random.php")
-      .then((res) => res.json())
-      .then((d) => setRandamfood(d.meals));
+    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+      .then(res => res.json())
+      .then(d => setRandomFood(d.meals));
   }, []);
+
   return (
     <div className="Header">
-      {randamfood.map((item) => {
-        return (
-          <section className="Hero" key={item.idMeal}>
-            <div className="Header1">
-              <h2 className="HeroTitle">Today Special</h2>
-              <h2>
-                <span className="OrangeText">{item.strMeal}</span>
-              </h2>
-              <h2 className="HeroSubtitle">{item.strCategory}</h2>
-            </div>
-            <div className="HeroImgWrapper">
-              <img
-                src={item.strMealThumb}
-                alt={item.strMeal}
-                className="HeroImg"
-              />
-            </div>
-          </section>
-        );
-      })}
+      {randomFood.map((item) => (
+        <section className="Hero" key={item.idMeal}>
+          <div className="Header1">
+            <h2 className="HeroTitle">Today Special</h2>
+            <h2><span className="OrangeText">{item.strMeal}</span></h2>
+            <h2 className="HeroSubtitle">{item.strCategory}</h2>
+          </div>
+          <div className="HeroImageWrapper">
+            <img src={item.strMealThumb} alt={item.strMeal} className="HeroImage" />
+          </div>
+        </section>
+      ))}
     </div>
   );
 };
